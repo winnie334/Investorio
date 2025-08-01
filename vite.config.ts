565a0f63d8a5ecc41dda3a3b5b453357db787c01
Damian <damian.vlaicu@gmail.com>
2025-08-01 11:58:35 +0200

    switched port to not collide with sketchmate

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 export default {
     server: {
         host: '0.0.0.0',
-        port: 3000,
+        port: 4000,
     },
     plugins: [glsl()],
     assetsInclude: ['**/*.glb', '**/*.fbx'],
